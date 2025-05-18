f5e078d7862953924b592ee7957c8c0f37c9e9d0
mdyeasinhn <mdyeasinhn@gmail.com>
2025-05-18 23:00:11 +0600

    project interface and model done

---
+++ backend/src/app/module/Project/project.interface.ts
@@ -0,0 +1,9 @@
+export type TProject = {
+    title: string;
+    content: string;
+    image: string;
+    category: string;
+    frontEndLink: string;
+    backEndLink: string;
+    liveLink: string;
+}
---
+++ backend/src/app/module/Project/project.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from 'mongoose';
+import { TProject } from './project.interface';
+
+const projectSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    frontEndLink: {
+        type: String,
+        required: true,
+    },
+    backEndLink: {
+        type: String,
+        required: true,
+    },
+    liveLink: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+
+const Project = mongoose.model<TProject>('Content', projectSchema);
+
+export default Project;
